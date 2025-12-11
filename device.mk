c744a3aa0dea9ea6986da0951d84317e15ef5c4e
Bruno Martins <bgcngm@gmail.com>
2025-12-11 13:11:33 +0000

    giulia: Remove powershare and wireless charging support
    Change-Id: I0b75615bb016da60288fa429f99a6f035a5df943

--- device.mk
+++ device.mk
@@ -37,10 +37,6 @@ PRODUCT_PACKAGES += \
     OPlusSystemUIResTarget \
     OPlusWifiResTarget
 
-# PowerShare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare-service.oplus
-
 # Regional properties
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/build.ROW.prop:$(TARGET_COPY_OUT_ODM)/etc/build.ROW.prop \
