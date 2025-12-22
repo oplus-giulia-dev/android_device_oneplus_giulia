7f5b61b56621a74f799b5d187bf6a3219e3c06b7
madmax7896 <madmax78968@gmail.com>
2025-12-22 13:54:36 +0000

    giulia: Build required HIDLs

--- device.mk
+++ device.mk
@@ -21,6 +21,11 @@ TARGET_SCREEN_WIDTH := 1264
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946652107814787.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0-service \
+    hwservicemanager
+
 # LiveDisplay
 $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
