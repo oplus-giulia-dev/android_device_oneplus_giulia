f12fe162ddd227194c890065a7f7cbf6bb5586a5
madmax7896 <madmax78968@gmail.com>
2025-12-22 13:37:52 +0000

    giulia: Initial device tree conversion
    Change-Id: I598e93d42815c222975b1372a6135e79f5917e4f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,21 +5,21 @@
 #
 
 # Partitions
-BOARD_SUPER_PARTITION_SIZE := 17179869184
+BOARD_SUPER_PARTITION_SIZE := 14266433536
 
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8650-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/waffle
+DEVICE_PATH := device/oneplus/giulia
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := OP5929L1,OP595DL1
+TARGET_OTA_ASSERT_DEVICE := OP5D3BL1
 
 # Display
-TARGET_SCREEN_DENSITY := 640
+TARGET_SCREEN_DENSITY := 560
 
 # Kernel
-TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_WAFFLE_DTB=y
+TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_GIULIA_DTB=y
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
@@ -30,4 +30,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/waffle/BoardConfigVendor.mk
+include vendor/oneplus/giulia/BoardConfigVendor.mk
