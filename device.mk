c3a457b5277ed98362d41eabad97fa7707702f3e
madmax7896 <madmax78968@gmail.com>
2026-01-15 11:38:23 +0000

    fixup! giulia: Set correct system props for EU variant

--- device.mk
+++ device.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.EU.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.EU.prop \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.IN.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.IN.prop \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.NA.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.NA.prop \
+    $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.ROW.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.ROW.prop \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/23867/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/23867/build.default.prop
 
 # Shipping API
